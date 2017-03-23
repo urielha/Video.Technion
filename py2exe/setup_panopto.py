e6b4af3d3ee77a101408bace0c34cf0802308f4a
Uriel Hai <urielha@users.noreply.github.com>
2017-03-23 10:24:16 +0200

    using real path

--- py2exe/setup_panopto.py
+++ py2exe/setup_panopto.py
@@ -2,4 +2,5 @@ from distutils.core import setup
 import py2exe
 from os import path
 
-setup(console=[path.join('..', 'download_panopto.py')])
+scriptPath = path.dirname(path.realpath(__file__))
+setup(console=[path.join(scriptPath, '..', 'download_panopto.py')])
