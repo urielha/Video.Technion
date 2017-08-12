20258485e7569713c0521b293d3bdf5ff9324bc4
Uriel Hai <urielha@users.noreply.github.com>
2017-08-13 00:10:05 +0300

    Final with args too

--- download_panopto.py
+++ download_panopto.py
@@ -8,8 +8,9 @@ import os
 import sys
 import io 
 
-# import argparse
+import argparse
 
+# ~~~~~~~~~~~~~~~~~~ encoding stuff ~~~~~~~~~~~~~~~
 # DOESN'T NEEDED: run in console: "chcp 1255" before the script
 # sys.stdout = codecs.getwriter('cp1255')(sys.stdout.buffer, 'strict') - didn't work well because the buffering mechanism
 
@@ -26,10 +27,11 @@ class stdoutWrapper(io.TextIOWrapper):
 		
 sys.stdout = stdoutWrapper('cp1255', 'strict')
 # print(sys.stdout.encoding)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 def download(url, toFile):
 	print(toFile)
-	print(u"downloading file: %s\n\t-> to: %s" % (url, toFile))
+	print(u"downloading file: {}\n\t-> to: {}".format(url, toFile))
 	chunkSize = 1024*512  
 	response = urlopen(url)
 	f = open(toFile, 'wb')
@@ -51,57 +53,70 @@ def readWeb(url):
 def getNumber(txt, bounds):
 	while(True):
 		try:
-			number = int(input(u"%s (%d-%d):" % (txt, bounds[0], bounds[1])))
+			number = int(input(u"{} ({}-{}):".format(txt, bounds[0], bounds[1])))
 		except ValueError:
 			print(u"ERROR: Please enter number!")
 			continue
 
 		if number < bounds[0] or number > bounds[1]:
-			print(u"ERROR: Please enter number from %d to %d" % (bounds[0], bounds[1]))
+			print(u"ERROR: Please enter number from {} to {}".format(bounds[0], bounds[1]))
 			continue
 		return number
 
 
-def main(rssURL):
-	content = readWeb(rssURL)
-
-	files = re.findall("<item>\s+<title>([^<]*)</title>.*?<guid>([^<]*)</guid>", content, re.DOTALL)
-	length = len(files)
-	startIndex = getNumber("start download from", (1,length))
-	stopIndex = getNumber("download till", (startIndex, length))
-	prefix = input("Add prefix to the file? (press enter for none)") or ""
-	outputDir = "."
-
-	for f in files[startIndex - 1 : stopIndex]:
-		dest = os.path.join(outputDir, u"%s%s.mp4" % (prefix, f[0]))
-		# if not os.path.isfile(dest):
-		download(f[1], dest)
-
 def getRSSUrl(coursePageUrl): 
-	urlComponents = re.match("https?://([^/]+)/Panopto.*folderID=(?:%22|\")([a-zA-Z0-9\-]{36})", coursePageUrl, re.I)
+	RE_COURSE_URL = "https?://([^/]+)/Panopto.*folderID=(?:%22|\")([a-zA-Z0-9\-]{36})"
+
+	urlComponents = re.match(RE_COURSE_URL, coursePageUrl, re.I)
 	if not urlComponents:
-		raise ValueError(u"Could not parse the course url!\n Make sure it looks like this:" + \
-						u"\n\thttps://panoptotech.cloud.panopto.eu/Panopto/Pages/Sessions/List.aspx#folderID=3cae3bb8-2422-4955-81db-44b63ecd63de" + \
-						u"(with http OR https at the beginning)")
+		print(u"Could not parse the course url!\n Make sure it looks like this:" + \
+				u"\n\thttps://panoptotech.cloud.panopto.eu/Panopto/Pages/Sessions/List.aspx#folderID=3cae3bb8-2422-4955-81db-44b63ecd63de" + \
+				u"\n\t(with http OR https at the beginning)")
+		return ''
 	panoptoUrl = urlComponents.group(1)
 	courseId = urlComponents.group(2)
 	return "http://%s/Panopto/Podcast/Podcast.ashx?courseid=%s&type=mp4" % (panoptoUrl, courseId)
-	
 
-if __name__ == '__main__':
-	print(u"Welcome to Panopto downloader [Home page: https://github.com/urielha/Video.Technion]")
+
+def main(arguments):
+	RE_VIDEO_RSS = "<item>\s+<title>([^<]*)</title>.*?<guid>([^<]*)</guid>"
+	global WELCOME
+	print(WELCOME)
 	print(u"")
-	print(u"Please enter the linke to the course (e.g. https://panoptotech.cloud.panopto.eu/Panopto/.../...folderID=**")
-	coursePageUrl = input(u"> ")
-	rssURL = getRSSUrl(coursePageUrl)
-	print(u"%s" % rssURL)
-	
-	main(rssURL)
 
-	# parser = argparse.ArgumentParser(description='Download videos one by one from panopto.', epilog='Written by uriel, hope you enjoy it.')
-	# parser.add_argument('rssURL', help='the RSS url of the Course in the Panopto')
-	# parser.add_argument('prefix', nargs='?', default='', help='prefix of the filename')
-	# parser.add_argument('-i', '--ignore', required=False, dest='ignoreTill', metavar='num', default=0, type=int, help='ignore x videos from the start (will automaticly ignore files that already exist)')
-	# parser.add_argument('-d', '--download', required=False, dest='downloadTill', metavar='num', default=-1, type=int, help='download x videos and stop (default is download all)')
-	# parser.add_argument('-o', '--output', required=False, dest='outputDir', metavar='output_dir', default='.', help='output directory (default is current)')
-	# arguments = parser.parse_args()
+	rssURL = '' if not arguments.courseUrl else getRSSUrl(arguments.courseUrl)
+	while not rssURL:
+		print(u"Please enter the link to the course (e.g. https://panoptotech.cloud.panopto.eu/Panopto/.../...folderID=**")
+		arguments.courseUrl = input(u"> ")
+		rssURL = getRSSUrl(arguments.courseUrl)
+	
+	content = readWeb(rssURL)
+
+	files = re.findall(RE_VIDEO_RSS, content, re.DOTALL)
+	length = len(files)
+	startIndex = arguments.startIndex if arguments.startIndex > 0 else getNumber("start download from", (1,length))
+	stopIndex = arguments.endIndex if arguments.endIndex > 0 else getNumber("download till", (startIndex, length))
+	prefix = arguments.prefix if arguments.prefix else input("Add prefix to the file? (press enter for none)") or ""
+	outputDir = arguments.outputDir
+
+	for f in files[max(0,startIndex - 1) : min(length, stopIndex)]:
+		dest = os.path.join(outputDir, u"{}{}.mp4".format(prefix, f[0]))
+		# if not os.path.isfile(dest):
+		download(f[1], dest)
+
+WELCOME = u"Welcome to Panopto downloader!\n[Home page: https://github.com/urielha/Video.Technion]"
+EPILOG = u"Written by Uriel"
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description=WELCOME, epilog=EPILOG)
+	parser.add_argument('courseUrl', nargs='?', default='', help='the url of the Course in the Panopto (should contain "folderID")')
+	parser.add_argument('-p', '--prefix', required=False, dest='prefix', default='', help='prefix of the filename')
+	parser.add_argument('-s', '--startIndex', required=False, dest='startIndex', metavar='NUM', default=0, type=int, help='index of the first video to download (default is the very first one)')
+	parser.add_argument('-e', '--endIndex', required=False, dest='endIndex', metavar='NUM', default=0, type=int, help='index of the last video (default is to download till the last one)')
+	parser.add_argument('-o', '--output', required=False, dest='outputDir', metavar='OUTPUT_DIR', default='.', help='output directory (default is current)')
+	arguments = parser.parse_args()
+	
+	try:
+		main(arguments)
+	except Exception as ex:
+		print(ex)
+	input("Press Enter to continue...")
