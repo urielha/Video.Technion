8826d99dfeee3f52c82ac8d13f8ddfb1310544c4
Uriel Hai <urielha@users.noreply.github.com>
2017-08-14 10:09:07 +0300

    Support output dir from input prompt

--- README.md
+++ README.md
@@ -15,6 +15,9 @@ Pay attention to run the script with **Python 3.*** (or use the executable).
 
 If you are using the executable make sure to extract all files from the **download_panopto.zip** 
 
+### >Hebrew note
+If you want to change the output path (or any other parameter) to a path that contains Hebrew please specify it as command parameter (see arguments below) with quotation marks and not at the input prompt itself.
+
 ## Usage with arguments
 
 usage: download_panopto.py courseUrl \[-h\] \[-p PREFIX\] \[-s NUM\] \[-e NUM\] \[-o OUTPUT_DIR\]
@@ -29,9 +32,8 @@ usage: download_panopto.py courseUrl \[-h\] \[-p PREFIX\] \[-s NUM\] \[-e NUM\] 
 | Argument | Description |
 | -------- | ----------- |
 | -h, --help | show this help message and exit |
-| :new: -s, --startIndex *num* | index of the first video to download (default is the very first one) |
-| :new: -e, --endIndex *num* | index of the last video (default is to download till the last one) |
-| -o, --output *output_dir* | output directory (default is current) |
+| :new: -s, --startIndex *num* | index of the first video to download |
+| :new: -e, --endIndex *num* | index of the last video |
+| -o, --output *output_dir* | output directory |
 | -p, --prefix *prefix* | prefix of the filename |
-
 
--- download_panopto.py
+++ download_panopto.py
@@ -4,9 +4,9 @@
 
 from urllib.request import urlopen
 import re
-import os 
+import os
 import sys
-import io 
+import io
 import ssl
 
 import argparse
@@ -18,105 +18,115 @@ import argparse
 # This class is to normalize filenames (such as hebrew) so they won't be scrumbled
 # Maybe we don't need this anymore
 class stdoutWrapper(io.TextIOWrapper):
-	def __init__(self, encoding, error):
-		super(stdoutWrapper, self).__init__(sys.stdout.buffer, encoding, 
-										    error, newline='\n', 
-										    write_through=True)
-	def write(self, s):
-		super(stdoutWrapper, self).write(s)
-		sys.stdout.flush()
-		
+    def __init__(self, encoding, error):
+        super(stdoutWrapper, self).__init__(sys.stdout.buffer, encoding,
+                                            error, newline='\n',
+                                            write_through=True)
+    def write(self, s):
+        super(stdoutWrapper, self).write(s)
+        sys.stdout.flush()
+
 sys.stdout = stdoutWrapper('cp1255', 'strict')
 # print(sys.stdout.encoding)
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 def download(url, toFile):
-	print(u"downloading file: {}\n\t-> to: {}".format(url, toFile))
-	chunkSize = 1024*512
-	gcontext = ssl.SSLContext(ssl.PROTOCOL_TLSv1) # don't verify certificate
-	response = urlopen(url, context=gcontext)
-	f = open(toFile, 'wb')
-	while True:
-		buff = response.read(chunkSize)
-		if not buff:
-			break
-		f.write(buff)
-	print(u"finish")
-	f.close()
-	response.close()
+    print(u"downloading file: {}\n\t-> to: {}".format(url, toFile))
+    chunkSize = 1024*512
+    gcontext = ssl.SSLContext(ssl.PROTOCOL_TLSv1) # don't verify certificate
+    response = urlopen(url, context=gcontext)
+    f = open(toFile, 'wb')
+    while True:
+        buff = response.read(chunkSize)
+        if not buff:
+            break
+        f.write(buff)
+    print(u"finish")
+    f.close()
+    response.close()
 
 def readWeb(url):
-	content = ""
-	with urlopen(url) as opener:
-		content = opener.read().decode()
-	return content
+    content = ""
+    with urlopen(url) as opener:
+        content = opener.read().decode()
+    return content
 
-def getNumber(txt, bounds):
-	while(True):
-		try:
-			number = int(input(u"{} ({}-{}):".format(txt, bounds[0], bounds[1])))
-		except ValueError:
-			print(u"ERROR: Please enter number!")
-			continue
+def getNumber(txt, bounds, default):
+    while True:
+        try:
+            numberStr = input(u"{} ({}-{}, default is {}): ".format(txt, bounds[0], bounds[1], default))
+            number = int(default if not numberStr else numberStr)
+        except ValueError:
+            print(u"ERROR: Please enter number!")
+            continue
 
-		if number < bounds[0] or number > bounds[1]:
-			print(u"ERROR: Please enter number from {} to {}".format(bounds[0], bounds[1]))
-			continue
-		return number
+        if number < bounds[0] or number > bounds[1]:
+            print(u"ERROR: Please enter number from {} to {}".format(bounds[0], bounds[1]))
+            continue
+        return number
 
 
 def getRSSUrl(coursePageUrl): 
-	RE_COURSE_URL = "https?://([^/]+)/Panopto.*folderID=(?:%22|\")?([a-zA-Z0-9\-]{36})"
+    RE_COURSE_URL = "https?://([^/]+)/Panopto.*folderID=(?:%22|\")?([a-zA-Z0-9\-]{36})"
 
-	urlComponents = re.match(RE_COURSE_URL, coursePageUrl, re.I)
-	if not urlComponents:
-		print(u"Could not parse the course url!\n Make sure it looks like this:" + \
-				u"\n\thttps://panoptotech.cloud.panopto.eu/Panopto/Pages/Sessions/List.aspx#folderID=3cae3bb8-2422-4955-81db-44b63ecd63de" + \
-				u"\n\t(with http OR https at the beginning)")
-		return ''
-	panoptoUrl = urlComponents.group(1)
-	courseId = urlComponents.group(2)
-	return "http://{}/Panopto/Podcast/Podcast.ashx?courseid={}&type=mp4".format(panoptoUrl, courseId)
+    urlComponents = re.match(RE_COURSE_URL, coursePageUrl, re.I)
+    if not urlComponents:
+        print(u"Could not parse the course url!\n Make sure it looks like this:" + \
+                u"\n\thttps://panoptotech.cloud.panopto.eu/Panopto/Pages/Sessions/List.aspx#folderID=3cae3bb8-2422-4955-81db-44b63ecd63de" + \
+                u"\n\t(with http OR https at the beginning)")
+        return ''
+    panoptoUrl = urlComponents.group(1)
+    courseId = urlComponents.group(2)
+    return "http://{}/Panopto/Podcast/Podcast.ashx?courseid={}&type=mp4".format(panoptoUrl, courseId)
 
 
 def main(arguments):
-	RE_VIDEO_RSS = "<item>\s+<title>([^<]*)</title>.*?<guid>([^<]*)</guid>"
-	global WELCOME
-	print(WELCOME)
+    RE_VIDEO_RSS = "<item>\s+<title>([^<]*)</title>.*?<guid>([^<]*)</guid>"
+    global WELCOME
+    print(WELCOME)
 
-	rssURL = '' if not arguments.courseUrl else getRSSUrl(arguments.courseUrl)
-	while not rssURL:
-		print(u"\nPlease enter the link to the course (e.g. https://panoptotech.cloud.panopto.eu/Panopto/.../...folderID=**")
-		arguments.courseUrl = input(u"> ")
-		rssURL = getRSSUrl(arguments.courseUrl)
-	
-	print("")
-	content = readWeb(rssURL)
+    rssURL = '' if not arguments.courseUrl else getRSSUrl(arguments.courseUrl)
+    while not rssURL:
+        print(u"\nPlease enter the link to the course (e.g. https://panoptotech.cloud.panopto.eu/Panopto/.../...folderID=**")
+        arguments.courseUrl = input(u"> ")
+        rssURL = getRSSUrl(arguments.courseUrl)
+    
+    print("")
+    content = readWeb(rssURL)
 
-	files = re.findall(RE_VIDEO_RSS, content, re.DOTALL)
-	length = len(files)
-	startIndex = arguments.startIndex if arguments.startIndex > 0 else getNumber("start download from", (1,length))
-	stopIndex = arguments.endIndex if arguments.endIndex > 0 else getNumber("download till", (startIndex, length))
-	prefix = arguments.prefix if arguments.prefix else input("Add prefix to the file? (press enter for none) ") or ""
-	outputDir = arguments.outputDir
+    files = re.findall(RE_VIDEO_RSS, content, re.DOTALL)
+    length = len(files)
+    startIndex = arguments.startIndex if arguments.startIndex > 0 else getNumber("Start download from", (1, length), 1)
+    stopIndex = arguments.endIndex if arguments.endIndex > 0 else getNumber("Download till", (startIndex, length), length)
+    outputDir = arguments.outputDir if arguments.outputDir else input("Output directory (press Enter for current dir: {}) ".format(os.getcwd()))
+    prefix = arguments.prefix if arguments.prefix else input("Add prefix to the files? (press Enter for none) ") or ""
 
-	for f in files[startIndex - 1 : stopIndex]:
-		dest = os.path.join(outputDir, u"{}{}.mp4".format(prefix, f[0]))
-		download(f[1], dest)
+    # Start downloading!
+    for f in files[startIndex - 1 : stopIndex]:
+        dest = os.path.join(outputDir, u"{}{}.mp4".format(prefix, f[0]))
+        download(f[1], dest)
 
 WELCOME = u"Welcome to Panopto downloader!\n[Home page: https://github.com/urielha/Video.Technion]"
 EPILOG = u"Written by Uriel"
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser(description=WELCOME, epilog=EPILOG)
-	parser.add_argument('courseUrl', nargs='?', default='', help='the url of the Course in the Panopto (should contain "folderID")')
-	parser.add_argument('-p', '--prefix', required=False, dest='prefix', default='', help='prefix of the filename')
-	parser.add_argument('-s', '--startIndex', required=False, dest='startIndex', metavar='NUM', default=0, type=int, help='index of the first video to download (default is the very first one)')
-	parser.add_argument('-e', '--endIndex', required=False, dest='endIndex', metavar='NUM', default=0, type=int, help='index of the last video (default is to download till the last one)')
-	parser.add_argument('-o', '--output', required=False, dest='outputDir', metavar='OUTPUT_DIR', default='.', help='output directory (default is current)')
-	arguments = parser.parse_args()
-	
-	try:
-		main(arguments)
-	except Exception as ex:
-		print(ex)
-	input("Press Enter to continue...")
+    parser = argparse.ArgumentParser(description=WELCOME, epilog=EPILOG)
+    parser.add_argument('courseUrl',
+                        nargs='?', default='', help='the url of the Course in the Panopto (should contain "folderID")')
+    parser.add_argument('-p', '--prefix',
+                        required=False, dest='prefix', default='', help='prefix of the filename')
+    parser.add_argument('-s', '--startIndex',
+                        required=False, dest='startIndex', metavar='NUM', default=0, type=int,
+                        help='index of the first video to download')
+    parser.add_argument('-e', '--endIndex',
+                        required=False, dest='endIndex', metavar='NUM', default=0, type=int,
+                        help='index of the last video')
+    parser.add_argument('-o', '--output',
+                        required=False, dest='outputDir', metavar='OUTPUT_DIR', default='',
+                        help='output directory')
+    arguments = parser.parse_args()
+    
+    try:
+        main(arguments)
+    except Exception as ex:
+        print(ex)
+    input('Press Enter to continue...')
