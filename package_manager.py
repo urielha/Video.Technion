a0d4ab05db90d02b74973cc971e93d65bdebf3a4
Uriel Hai <urielha@users.noreply.github.com>
2018-03-26 22:26:52 +0300

    installing is working

--- package_manager.py
+++ package_manager.py
@@ -1,24 +1,38 @@
-
 def install_pip():
     try:
         import pip
+        return None
     except ImportError:
         # installing pip
-        if 'y' != input("pip is not installed, press 'y' to install: ").lower():
+        if "y" != input(
+                "pip is not installed, press 'y' to install: ").lower():
             input("Exiting - Press Enter to quit ")
             exit()
 
-        pipUrl = "https://bootstrap.pypa.io/get-pip.py"
+    pipUrl = "https://bootstrap.pypa.io/get-pip.py"
 
-        from urllib.request import urlopen
+    from urllib.request import urlopen
 
+    with open("get-pip.py", "wb") as getpip_py:
         with urlopen(pipUrl) as response:
-            getpip_py = response.read()
-            eval(getpip_py)
-            import pip
+            getpip_py.write(response.read())
+    from os import system, remove
+    from sys import executable as PythonExec
+    system("{} {}".format(PythonExec, getpip_py.name))
+    remove("get-pip.py")
+    print("")
 
 
 def install_req():
     import pip
-    pip.main(['install', '-q', '-r', 'requirements.txt'])
-    
+    try:
+        from selenium import webdriver
+        return None
+    except ImportError:
+        if "y" != input(
+                "selenium is not installed, press 'y' to install: ").lower():
+            input("Exiting - Press Enter to quit ")
+            exit()
+
+    pip.main(['install', '-r', 'requirements.txt'])
+    print("")
