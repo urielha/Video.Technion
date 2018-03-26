98a0db2496cbf0953e4d397cf6b62b907f799b8a
Uriel Hai <urielha@users.noreply.github.com>
2018-03-26 19:51:25 +0300

    first try

--- download_oldvideo.py
+++ download_oldvideo.py
@@ -6,14 +6,23 @@ from os.path import join as joinPath
 import traceback
 from collections import namedtuple
 
+def importSeleniumModules():
+    global webdriver, By, EC, Wait
+    try:
+        from selenium import webdriver
+        from selenium.webdriver.common.by import By
+        from selenium.webdriver.support import expected_conditions as EC
+        from selenium.webdriver.support.ui import WebDriverWait as Wait
+    except ImportError:
+        return False
+    return True
 
-try:
-    from selenium import webdriver
-    from selenium.webdriver.common.by import By
-    from selenium.webdriver.support import expected_conditions as EC
-    from selenium.webdriver.support.ui import WebDriverWait as Wait
-except ImportError:
-    pass
+if not importSeleniumModules():
+    import package_manager
+    package_manager.install_selenium()
+    if not importSeleniumModules():
+        print("Error getting required modules")
+        exit()
 
 Selectors = namedtuple("Selectors", ["links", "video_button"])
 InputsIds = namedtuple("InputsIds", ["name", "password", "server", "submit"])
